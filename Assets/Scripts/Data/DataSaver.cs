83fabd0540dc1f97fd110f657a8c876e5b92cd35
Ivan_Nikolaev <sampodelivery@gamil.com>
2024-01-25 19:30:55 +0700

    Mobile save fixed

--- Assets/Scripts/Data/DataSaver.cs
+++ Assets/Scripts/Data/DataSaver.cs
@@ -1,82 +1,22 @@
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System;
 using UnityEngine;
 
 public static class DataSaver
 {
-    public static void Save<T>(List<T> dataToSave, string fileName)
+    public static void Save(string key, PlayerData data)
     {
-        string context = JSonHelper.ToJSon<T>(dataToSave.ToArray());
-        WriteFile(GetPath(fileName), context);
-        Debug.Log("Saved to: " + GetPath(fileName));
+        string dataToSave = JsonUtility.ToJson(data, true);
+        PlayerPrefs.SetString(key, dataToSave);
     }
 
-    public static List<T> ReadFromJSON<T>(string fileName)
+    public static PlayerData Load(string key)
     {
-        string content = ReadFile(GetPath(fileName));
-        if (string.IsNullOrEmpty(content) || content == "{}")
+        if (PlayerPrefs.HasKey(key))
         {
-            return new List<T>();
+            string dataToLoad = PlayerPrefs.GetString(key);
+            return JsonUtility.FromJson<PlayerData>(dataToLoad);
         }
-        List<T> data = JSonHelper.FromJSon<T>(content).ToList();
-        return data;
-    }
+        else
+            return null;
 
-    private static string GetPath(string fileName)
-    {
-        return Path.Combine(Application.persistentDataPath, fileName);
-        //return Application.persistentDataPath + "/" + fileName;
-    }
-
-    private static void WriteFile(string path, string context)
-    {
-        FileStream fileStream = new FileStream(path, FileMode.Create);
-        using (StreamWriter writer = new StreamWriter(fileStream))
-        {
-            writer.Write(context);
-        }
-    }
-
-    private static string ReadFile(string path)
-    {
-        if (File.Exists(path))
-        {
-            using (StreamReader reader = new StreamReader(path))
-            {
-                string content = reader.ReadToEnd();
-                return content;
-            }
-        }
-        return "";
-    }
-}
-public static class JSonHelper
-{
-    public static T[] FromJSon<T>(string jSon)
-    {
-        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(jSon);
-        return wrapper.Items;
-    }
-
-    public static string ToJSon<T>(T[] array)
-    {
-        Wrapper<T> wrapper = new Wrapper<T>();
-        wrapper.Items = array;
-        return JsonUtility.ToJson(wrapper);
-    }
-
-    public static string ToJSon<T>(T[] array, bool prettyPrint)
-    {
-        Wrapper<T> wrapper = new Wrapper<T>();
-        wrapper.Items = array;
-        return JsonUtility.ToJson(wrapper, prettyPrint);
-    }
-
-    [Serializable]
-    private class Wrapper<T>
-    {
-        public T[] Items;
     }
 }
